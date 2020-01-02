746c5154181397f60b2d7b4beb2879ab47f2c47a
tong <newrtong@gmail.com>
2020-01-03 00:18:21 +0800

    Add schedule feature

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Schema;
+use App\Models\LineNotification;
+use App\Services\Line\LineNotify;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +29,41 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+
+        if (Schema::hasTable('line_notifications')) {
+            $notifications = LineNotification::where('status', 1)->get();
+            if ($notifications->count() > 0) {
+                $line_notify = new LineNotify(
+                    env('LINE_CLIENT_ID'),
+                    env('LINE_CLIENT_SECRET'),
+                    env('LINE_CALLBACK_URL')
+                );
+                foreach ($notifications as $notification) {
+                    $schedule->call(function () use ($notification, &$line_notify) {
+
+                        $message = "\r\n" . $notification->message;
+                        $displayText = '';
+                        if ($notification->display_date != -1) {
+                            $timestamps = strtotime("+{$notification->display_date} days");
+                            $date = date('Y/m/d', $timestamps);
+                            $week = ['日', '一', '二', '三', '四', '五', '六'][date('w', $timestamps)];
+
+                            $displayText = "{$date}({$week}) " . $notification->display_time;
+                            $message = $message . "\r\n\r\n" . "時間：" . $displayText;
+                        }
+
+                        if ($notification->is_not_reply) {
+                            $message = $message . "\r\n\r\n***此訊息為系統自動發出，請勿回覆***";
+                        }
+                        $data = [
+                            'message' => $message,
+                        ];
+                        $line_notify->sendNotify($notification->token, $data);
+                    })->cron($notification->cron);
+                }
+            }
+        }
     }
 
     /**
@@ -35,7 +73,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
